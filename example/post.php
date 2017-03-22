42169437d3e6cd16904fe90b1a7d54d2f7afd544
Alexey Baranov <alexey.baranov@outlook.com>
2017-03-22 23:38:03 +0300

    initial commit of the library, v.0.1

--- example/post.php
+++ example/post.php
@@ -13,6 +13,7 @@ use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
 
 $config = include "config.php";
+
 $taskId = 1087;
 $serviceDeskId = 7375;
 $content = '<table><thead><tr><th>asd <br></th><th>&nbsp;</th><th>&nbsp;</th><th>&nbsp;</th></tr></thead><tbody><tr><td><br></td><td>asd<br></td><td><br></td><td><br></td></tr><tr><td><br></td><td><br></td><td><br></td><td><br></td></tr></tbody></table><br>';
